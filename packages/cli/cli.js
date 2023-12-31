f78b3b87aaba98c3c69ac32a62e7f1ab9c0cfee8
DemoZeKai <zekaiyangdemo@gmail.com>
2023-12-31 14:50:17 +0800

    feat: packages下新增了脚手架

---
+++ packages/cli/cli.js
@@ -0,0 +1,100 @@
+import commandLineArgs from 'command-line-args';
+import { readFile } from 'fs/promises';
+import commandLineUsage from 'command-line-usage';
+import prompts from 'prompts';
+import gitClone from './utils/gitClone.js';
+const pkg = JSON.parse(
+  await readFile(new URL('./package.json', import.meta.url))
+);
+//配置命令参数
+const optionDefinitions = [
+  { name: 'version', alias: 'v', type: Boolean },
+  {
+    name: 'help',
+    alias: 'h',
+    type: Boolean
+  }
+];
+
+//帮助命令
+const helpSections = [
+  {
+    header: 'create-orangeui',
+    content: '一个快速生成组件库搭建环境的脚手架'
+  },
+  {
+    header: 'Options',
+    optionList: [
+      {
+        name: 'version',
+        alias: 'v',
+        typeLabel: '{underline boolean}',
+        description: '版本号'
+      },
+      {
+        name: 'help',
+        alias: 'h',
+        typeLabel: '{underline boolean}',
+        description: '帮助'
+      }
+    ]
+  }
+];
+
+const promptsOptions = [
+  {
+    type: 'text',
+    name: 'name',
+    message: '请输入项目名称'
+  },
+  {
+    type: 'select', //单选
+    name: 'template',
+    message: '请选择一个模板',
+    choices: [
+      { title: 'github-or-ui', value: 1 },
+      {
+        title: 'gitee-or-ui',
+        value: 2
+      }
+    ]
+  }
+];
+
+const options = commandLineArgs(optionDefinitions);
+
+//cli.js
+const remoteList = {
+  1: 'https://github.com/DemoZeKaiYang/or-ui.git', //git要改成master
+  2: 'https://gitee.com/yzklai/or-ui.git' //git要改成master
+};
+
+const getUserInfo = async () => {
+  const res = await prompts(promptsOptions);
+
+  if (!res.name || !res.template) return;
+  gitClone(
+    `direct:${remoteList[res.template]}`,
+    res.name,
+    {
+      clone: true
+    },
+    function (err) {
+      console.log(err ? 'Error' : 'Success');
+    }
+  );
+};
+
+const runOptions = () => {
+  if (options.version) {
+    console.log(`v${pkg.version}`);
+    return;
+  }
+  if (options.help) {
+    console.log(commandLineUsage(helpSections));
+    return;
+  }
+  getUserInfo();
+};
+
+runOptions();
